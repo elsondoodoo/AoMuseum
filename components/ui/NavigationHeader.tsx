ddcd8b227f60b6927260052aabeaa2874d480f5a
Ingan <ingan00001@gmail.com>
2024-11-24 16:24:51 -0800

    Update NavigationHeader.tsx

--- components/ui/NavigationHeader.tsx
+++ components/ui/NavigationHeader.tsx
@@ -2,22 +2,23 @@
 import React from 'react';
 import Link from 'next/link';
 import { ModeToggle } from "@/components/theme-toggle";
+import { ConnectButton } from "arweave-wallet-kit";
 
 const NavigationHeader: React.FC = () => {
-  
-  return (
-    <header className="w-full bg-background border-b border-border">
-      <nav className="container mx-auto px-4 py-4 flex justify-between items-center">
-      <Link href="/" className="text-xl font-bold hover:scale-105 hover:text-orange-500 transition-transform transition-colors duration-500">Arweave Museum</Link>
 
-        <ul className="flex space-x-4 items-center">
-          <li>
-            <ModeToggle />
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+    return (
+        <header className="w-full bg-background border-b border-border">
+            <nav className="container mx-auto px-4 py-4 flex justify-between items-center">
+                <Link href="/" className="text-xl font-bold hover:scale-105 hover:text-orange-500 transition-transform transition-colors duration-500">Arweave Museum</Link>
+                <ul className="flex space-x-4 items-center">
+                    <ConnectButton className='ml-4' />
+                    <li>
+                        <ModeToggle />
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 };
 
 export default NavigationHeader;
