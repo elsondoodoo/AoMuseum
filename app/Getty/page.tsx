0e0e35b0d8ac001e83e8fe8f52dcbeb964c10cae
Olivli <olivli148@gmail.com>
2024-11-24 16:55:47 -0800

    kind of fixed styling

--- app/Getty/page.tsx
+++ app/Getty/page.tsx
@@ -16,27 +16,30 @@ import {
 import { UploadDialog } from "@/components/Upload"
 export default function Home() {
     return (
-        <div className="flex justify-center">
+        <div className="flex flex-col">
             <div>
                 <NavigationHeader />
             </div>
-            <Carousel className="w-full max-w-xs">
-                <CarouselContent>
-                    {Array.from({ length: 5 }).map((_, index) => (
-                    <CarouselItem key={index}>
-                        <div className="p-1">
-                        <Card>
-                            <CardContent className="flex aspect-square items-center justify-center p-6">
-                            <span className="text-4xl font-semibold">{index + 1}</span>
-                            </CardContent>
-                        </Card>
-                        </div>
-                    </CarouselItem>
-                    ))}
-                </CarouselContent>
-                <CarouselPrevious />
-                <CarouselNext />
-            </Carousel>
+            <div className="items-center">
+                <Carousel className="w-full max-w-xs">
+                    <CarouselContent>
+                        {Array.from({ length: 5 }).map((_, index) => (
+                        <CarouselItem key={index}>
+                            <div className="p-1">
+                            <Card>
+                                <CardContent className="flex aspect-square items-center justify-center p-6">
+                                <span className="text-4xl font-semibold">{index + 1}</span>
+                                </CardContent>
+                            </Card>
+                            </div>
+                        </CarouselItem>
+                        ))}
+                    </CarouselContent>
+                    <CarouselPrevious />
+                    <CarouselNext />
+                </Carousel>
+            </div>
+            
             <UploadDialog/>
         </div>
     );
