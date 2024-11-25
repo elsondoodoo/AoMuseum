9cbe5e3bf4c53f62bb1262d5dff9b010d5aa1c95
Ingan <ingan00001@gmail.com>
2024-11-24 16:45:36 -0800

    add navigation header

--- app/Getty/page.tsx
+++ app/Getty/page.tsx
@@ -1,7 +1,11 @@
 "use client"
+import NavigationHeader from "@/components/ui/NavigationHeader";
 export default function Home() {
     return (
         <div>
+            <div>
+                <NavigationHeader />
+            </div>
             <h1>Hello, World!</h1>
         </div>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -4,28 +4,23 @@ import NavigationHeader from "@/components/ui/NavigationHeader";
 import Map from "@/components/map";
 import { UploadDialog } from "@/components/Upload"
 import Link from 'next/link';
-import { ArweaveWalletKit } from "arweave-wallet-kit";
 
 export default function Home() {
-    return (
-        <div>
-            <ArweaveWalletKit config={{
-                permissions: ["ACCESS_ADDRESS", "SIGN_TRANSACTION"],
-                ensurePermissions: true
-            }}>
-                <div>
-                    <NavigationHeader />
-                </div>
+  return (
+    <div>
+      <div>
+        <NavigationHeader />
+      </div>
 
-                <div className="flex justify-center ml-10 mr-10">
-                    <div className="w-full p-10">
-                        <Map />
-                    </div>
-                </div>
-                <div className="flex justify-center ml-10 mr-10 mb-10">
-                    <UploadDialog />
-                </div>
-            </ArweaveWalletKit>
+      <div className="flex justify-center ml-10 mr-10">
+        <div className="w-full p-10">
+          <Map />
         </div>
-    );
+      </div>
+      <div className="flex justify-center ml-10 mr-10 mb-10">
+        <UploadDialog />
+      </div>
+      
+    </div>
+  );
 }
